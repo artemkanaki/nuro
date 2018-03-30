69c54c19537093cc0caf146473a883ab2e9336a9
Artem Kanaki <kanaki.artem@gmail.com>
2018-03-30 22:58:06 +0300

    dist fix

--- dist/adaptive-resonance/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const binary_1 = require("./binary");
-exports.BinaryAdaptiveResonance = binary_1.BinaryAdaptiveResonance;
-const analog_1 = require("./analog");
-exports.AnalogAdaptiveResonance = analog_1.AnalogAdaptiveResonance;
-//# sourceMappingURL=index.js.map
